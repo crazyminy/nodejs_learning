68b498864e5f88c5335ff782d92d7d197141a7e4
fuzhenjie <crazyminy4131@gmail.com>
2019-08-27 15:48:53 +0800

    m

---
+++ server/handler.js
@@ -0,0 +1,15 @@
+function home(){
+    console.log('home');
+}
+function review(){
+    console.log('review');
+}
+function api(){
+    console.log("api");
+}
+
+module.export = {
+    home:home,
+    review:review,
+    api:api
+}
---
+++ server/router.js
@@ -0,0 +1,5 @@
+function route(pathName){
+    console.log('pathName'+pathName);
+}
+
+module.exports.route = route;
